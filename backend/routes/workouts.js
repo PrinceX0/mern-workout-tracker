4231548b45e2940f8ca4bf513d83cc27eab48439
PrinceXO <tr3xprincexo@gmail.com>
2026-06-02 12:00:00 +0530

    Add API routes and controller logic

---
+++ backend/routes/workouts.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
+
+
+const router = express.Router()
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+//GET a single workout
+router.get('/:id', getWorkout)
+
+//POST a new workout
+// router.post('/', async (req, res) => {
+//     // const { title, load, reps } = req.body
+//     // try {
+//     //     const workout = await Workout.create({ title, load, reps })
+//     //     res.status(200).json(workout)
+//     // } catch (error) {
+//     //     res.status(400).json({ error: error.message })
+//     // }
+//     // res.json({ mssg: 'POST new workout' })
+// })
+router.post('/', createWorkout)
+
+//DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+//UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+
+module.exports = router
